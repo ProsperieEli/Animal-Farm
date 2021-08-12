e3d82a796d96be5e90b0607092a38ec75305a14a
ProsperieEli <ElijahProsperie@gmail.com>
2021-08-11 21:01:04 -0400

    Work in progress. Horse complete!

--- app.js
+++ app.js
@@ -6,3 +6,12 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  const horseNoise = document.getElementById('Horse-Sound');
+  const myHorse = document.getElementById('Horse-img');
+  const noise = document.getElementById('Noise');
+
+
+  noise.addEventListener('click', () => {
+    horseNoise.play()
+  });
